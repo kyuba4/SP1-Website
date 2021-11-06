4230526fff23d6e92d54f6d52395f3e5fe20cd6c
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2021-11-06 19:29:32 +0100

    card styles moved to actual component

--- src/views/Panel.vue
+++ src/views/Panel.vue
@@ -76,35 +76,6 @@ export default {
             margin: 20px auto;
          }
       }
-
-      .card {
-         box-shadow: 0 0 10px 0 #909090;
-         border-radius: 25px;
-         transition: 300ms;
-         max-width: 450px;
-
-         &:hover {
-            transform: scale(1.05);
-            box-shadow: 0 0 1px 0 #909090;
-         }
-
-         img {
-            max-width: 100px;
-            margin-bottom: 15px;
-         }
-
-         .link {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            padding: 30px 50px;
-
-            span {
-               white-space: nowrap;
-            }
-         }
-      }
    }
 }
 
